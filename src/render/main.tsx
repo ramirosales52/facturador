b5d24e1bb13f8d2846c257c935e84e45b2370044
Ramiro Rosales <ramirosales52@gmail.com>
2025-10-06 21:01:21 -0300

    fix: use HashRouter instead of BrowserRouter for Electron compatibility

--- src/render/main.tsx
+++ src/render/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
